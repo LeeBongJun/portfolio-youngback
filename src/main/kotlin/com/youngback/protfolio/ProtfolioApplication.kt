fffed3398a77aa193a80554f5c420f32c6454ecb
bjl0615 <bjl0615@gmail.com>
2025-02-04 21:41:28 +0900

    First Commit

---
+++ src/main/kotlin/com/youngback/protfolio/ProtfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.youngback.protfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProtfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProtfolioApplication>(*args)
+}
---
+++ src/test/kotlin/com/youngback/protfolio/ProtfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.youngback.protfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProtfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
