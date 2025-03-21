c7f40acb30e9107e2ee9645cd1a824611a563dc4
bjl0615 <bjl0615@gmail.com>
2025-03-21 18:09:16 +0900

    컨틀롤러 개발

--- src/main/kotlin/com/youngback/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/youngback/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,17 +1,46 @@
 package com.youngback.portfolio.presentation.controller
 
+import com.youngback.portfolio.presentation.dto.IntroductionDTO
+import com.youngback.portfolio.presentation.dto.LinkDTO
+import com.youngback.portfolio.presentation.dto.ProjectDTO
+import com.youngback.portfolio.presentation.dto.ResumeDTO
+import com.youngback.portfolio.presentation.service.PresentationService
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
 import kotlin.coroutines.RestrictsSuspension
 
-@RestrictsSuspension
+@RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK"
+    }
+
+    @GetMapping("/v1/introductions")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/links")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
     }
 
 }
