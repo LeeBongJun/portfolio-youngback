45ec7913172ae9a8094bfe43378d7c7c7ab1eab9
bjl0615 <bjl0615@gmail.com>
2025-04-02 17:44:23 +0900

    인터셉터 개발

--- src/main/kotlin/com/youngback/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/youngback/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,25 @@
 package com.youngback.portfolio.presentation.interceptor
 
+import com.youngback.portfolio.domain.entity.HttpInterface
+import com.youngback.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
