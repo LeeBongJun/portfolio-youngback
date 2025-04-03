6af1f5832417888c85889a76be9de6be3d778e28
bjl0615 <bjl0615@gmail.com>
2025-04-03 18:10:12 +0900

    삽입, 수정 API 개발 - 연관 관계 없음

--- src/main/kotlin/com/youngback/portfolio/admin/context/introduction/service/AdminIntroductionService.kt
+++ src/main/kotlin/com/youngback/portfolio/admin/context/introduction/service/AdminIntroductionService.kt
@@ -1,9 +1,12 @@
 package com.youngback.portfolio.admin.context.introduction.service
 
+import com.youngback.portfolio.admin.context.introduction.form.IntroductionForm
 import com.youngback.portfolio.admin.data.TableDTO
 import com.youngback.portfolio.domain.entity.Introduction
 import com.youngback.portfolio.domain.repository.IntroductionRepository
+import com.youngback.portfolio.presentation.dto.IntroductionDTO
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminIntroductionService(
@@ -15,6 +18,18 @@ class AdminIntroductionService(
         val entities = introductionRepository.findAll()
 
         return TableDTO.from(classInfo , entities)
+    }
+
+    @Transactional
+    fun save(form : IntroductionForm) {
+        val introduction = form.toEntity()
+        introductionRepository.save(introduction)
+    }
+
+    @Transactional
+    fun update(id : Long , form : IntroductionForm) {
+        val introduction = form.toEntity(id)
+        introductionRepository.save(introduction)
     }
 
 }
--- src/main/kotlin/com/youngback/portfolio/admin/context/skill/service/AdminSkillService.kt
+++ src/main/kotlin/com/youngback/portfolio/admin/context/skill/service/AdminSkillService.kt
@@ -1,9 +1,12 @@
 package com.youngback.portfolio.admin.context.skill.service
 
+import com.youngback.portfolio.admin.context.link.form.LinkForm
+import com.youngback.portfolio.admin.context.skill.form.SkillForm
 import com.youngback.portfolio.admin.data.TableDTO
 import com.youngback.portfolio.domain.entity.Skill
 import com.youngback.portfolio.domain.repository.SkillRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminSkillService(
@@ -15,6 +18,18 @@ class AdminSkillService(
         val entities = skillRepository.findAll()
 
         return TableDTO.from(classInfo , entities)
+    }
+
+    @Transactional
+    fun save(form: SkillForm) {
+        val link = form.toEntity()
+        skillRepository.save(link)
+    }
+
+    @Transactional
+    fun update(id: Long, form: SkillForm) {
+        val link = form.toEntity(id)
+        skillRepository.save(link)
     }
 
 }
